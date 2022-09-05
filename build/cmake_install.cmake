798f36d9739b936e8c50fd5670791d43b3d3e97f
AdelinoGP <agpene@hotmail.com>
2022-09-05 03:19:10 -0300

    Alterações para gerar build

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/agpen/Desktop/Projeto_Programacao2
+# Install script for directory: C:/Users/agpen/Desktop/projeto_programacao_2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/agpen/Desktop/Projeto_Programacao2/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/agpen/Desktop/projeto_programacao_2/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
